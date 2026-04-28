0bf10d9703b0455a42ebd14175ec18c2dd548528
lgc202 <48577411+lgc202@users.noreply.github.com>
2026-04-28 16:18:31 +0800

    feat: add gateway rest storage

---
+++ internal/apiserver/registry/gateway/strategy.go
@@ -0,0 +1,82 @@
+package gateway
+
+import (
+	"context"
+
+	apiequality "k8s.io/apimachinery/pkg/api/equality"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/validation/field"
+	"k8s.io/apiserver/pkg/storage/names"
+	"sigs.k8s.io/structured-merge-diff/v6/fieldpath"
+
+	gatewayv1 "github.com/lgc202/ingate-next/pkg/apis/gateway/v1"
+)
+
+// strategy 定义 Gateway 资源在 apiserver 存储前后的处理规则
+type strategy struct {
+	runtime.ObjectTyper
+	names.NameGenerator
+}
+
+func newStrategy(typer runtime.ObjectTyper) strategy {
+	return strategy{
+		ObjectTyper:   typer,
+		NameGenerator: names.SimpleNameGenerator,
+	}
+}
+
+func (strategy) NamespaceScoped() bool {
+	return false
+}
+
+func (strategy) GetResetFields() map[fieldpath.APIVersion]*fieldpath.Set {
+	return map[fieldpath.APIVersion]*fieldpath.Set{
+		fieldpath.APIVersion(gatewayv1.SchemeGroupVersion.String()): fieldpath.NewSet(
+			fieldpath.MakePathOrDie("status"),
+		),
+	}
+}
+
+func (strategy) PrepareForCreate(ctx context.Context, obj runtime.Object) {
+	gateway := obj.(*gatewayv1.Gateway)
+	gateway.Status = gatewayv1.ResourceStatus{}
+	gateway.Generation = 1
+}
+
+func (strategy) Validate(ctx context.Context, obj runtime.Object) field.ErrorList {
+	return nil
+}
+
+func (strategy) WarningsOnCreate(ctx context.Context, obj runtime.Object) []string {
+	return nil
+}
+
+func (strategy) Canonicalize(obj runtime.Object) {
+}
+
+func (strategy) AllowCreateOnUpdate() bool {
+	return false
+}
+
+func (strategy) PrepareForUpdate(ctx context.Context, obj, old runtime.Object) {
+	newGateway := obj.(*gatewayv1.Gateway)
+	oldGateway := old.(*gatewayv1.Gateway)
+
+	newGateway.Status = oldGateway.Status
+	newGateway.Generation = oldGateway.Generation
+	if !apiequality.Semantic.DeepEqual(oldGateway.Spec, newGateway.Spec) {
+		newGateway.Generation = oldGateway.Generation + 1
+	}
+}
+
+func (strategy) ValidateUpdate(ctx context.Context, obj, old runtime.Object) field.ErrorList {
+	return nil
+}
+
+func (strategy) WarningsOnUpdate(ctx context.Context, obj, old runtime.Object) []string {
+	return nil
+}
+
+func (strategy) AllowUnconditionalUpdate() bool {
+	return false
+}
