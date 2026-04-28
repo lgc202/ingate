a00856f05501391f75358a6e44f4a978c33bd19e
liguangcai <liguangcai@ouryun.com.cn>
2026-04-28 15:49:08 +0800

    feat: add apiserver generic options

---
+++ internal/apiserver/server/options.go
@@ -0,0 +1,71 @@
+package server
+
+import (
+	"io"
+
+	"github.com/spf13/pflag"
+	utilerrors "k8s.io/apimachinery/pkg/util/errors"
+	genericapiserver "k8s.io/apiserver/pkg/server"
+	genericoptions "k8s.io/apiserver/pkg/server/options"
+
+	gatewayv1 "github.com/lgc202/ingate-next/pkg/apis/gateway/v1"
+)
+
+const (
+	// DefaultEtcdPathPrefix 表示 ingate-apiserver 在 etcd 中使用的默认前缀
+	DefaultEtcdPathPrefix = "/registry/gateway.ingate.io"
+)
+
+// Options 表示 ingate-apiserver 启动参数
+type Options struct {
+	ServerRunOptions   *genericoptions.ServerRunOptions
+	RecommendedOptions *genericoptions.RecommendedOptions
+
+	StdOut io.Writer
+	StdErr io.Writer
+}
+
+// NewOptions 创建 ingate-apiserver 默认启动参数
+func NewOptions(stdout, stderr io.Writer) *Options {
+	return &Options{
+		ServerRunOptions: genericoptions.NewServerRunOptions(),
+		RecommendedOptions: genericoptions.NewRecommendedOptions(
+			DefaultEtcdPathPrefix,
+			Codecs.LegacyCodec(gatewayv1.SchemeGroupVersion),
+		),
+		StdOut: stdout,
+		StdErr: stderr,
+	}
+}
+
+// AddFlags 注册 ingate-apiserver 命令行参数
+func (o *Options) AddFlags(flags *pflag.FlagSet) {
+	o.ServerRunOptions.AddUniversalFlags(flags)
+	o.RecommendedOptions.AddFlags(flags)
+}
+
+// Validate 校验 ingate-apiserver 启动参数
+func (o *Options) Validate() error {
+	var errs []error
+	errs = append(errs, o.ServerRunOptions.Validate()...)
+	errs = append(errs, o.RecommendedOptions.Validate()...)
+	return utilerrors.NewAggregate(errs)
+}
+
+// Complete 补全 ingate-apiserver 启动参数
+func (o *Options) Complete() error {
+	return o.ServerRunOptions.Complete()
+}
+
+// Config 创建 Kubernetes generic apiserver 推荐配置
+func (o *Options) Config() (*genericapiserver.RecommendedConfig, error) {
+	config := genericapiserver.NewRecommendedConfig(Codecs)
+	if err := o.ServerRunOptions.ApplyTo(&config.Config); err != nil {
+		return nil, err
+	}
+	if err := o.RecommendedOptions.ApplyTo(config); err != nil {
+		return nil, err
+	}
+
+	return config, nil
+}
